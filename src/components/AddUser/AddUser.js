5f65150b31eeac75792b28fe396bb28068b085e2
choyondas <choyondas08@gmail.com>
2022-03-08 22:18:29 +0800

    test

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,54 +1,54 @@
 import React from 'react';
 import { useRef } from 'react';
 const AddUser = () => {
+const nameRef = useRef();
+const emailRef = useRef();
 
-    const nameRef = useRef();
-    const emailRef = useRef();
-const handleAddUser =e=>{
-    e.preventDefault()
-const name = nameRef.current.value;
-const email = emailRef.current.value;
-const newUser = {name, email}
+
+
+const addHandleUser=e=>{
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const newUser ={name, email}
+
+   fetch('http://localhost:5000/users', {
+       method:'POST',
+       headers:{
+           'content-type':'application/json' 
+       },
+       body:JSON.stringify(newUser)
+   })
+   .then(res=>res.json())
+   .then(data=>{
+       if(data.insertedId){
+           alert('user added successfully')
+           e.target.reset();
+       }
+   })
+    
+
+
 
 console.log(newUser)
-
-
-
-fetch('http://localhost:5000/users', {
-    method:'POST',
-    headers:{
-        'content-type':'application/json'
-    },
-    body:JSON.stringify(newUser)
-})
-.then(res=>res.json())
-.then(data=>{
-    if (data.insertedId){
-        alert('User Added successfully')
-        e.target.reset();
-    }
-})
-    // console.log(nameRef);
-    // console.log(emailRef)
-
-
-
-
+e.preventDefault();
 
 }
-
-
 
     return (
         <div>
             <h2>This is Add User</h2>
 
-            <form onSubmit={handleAddUser}>
-                
-                <input ref={nameRef} type="text" name="" id=""placeholder='your name' /> <br />
-                <input ref={emailRef} type="text"  placeholder='your email' /> <br />
-                <input type="submit" value="submit" />
-                </form>
+          
+
+<form onSubmit={addHandleUser} >
+
+
+
+    <input type="text" placeholder='Your Name' ref={nameRef} /><br />
+    <input type="text" placeholder='Your Email'ref={emailRef}/> <br />
+    <input type="submit" value="submit" />
+</form>
+
         </div>
     );
 };
